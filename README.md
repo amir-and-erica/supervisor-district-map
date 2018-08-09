0c8ab73a1cb9e57bd1672680c2c27b21ed95b148
Jimmy Chion <ajimmykid@gmail.com>
2018-08-08 20:03:09 -0700

    webpack version

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # SF District Supervisor map with Mapbox
+For https://www.bythebay.cool/supervisor-map/
+
 Mapbox base map:  [Here](https://api.mapbox.com/styles/v1/bythebaydotcool/cjkk9y9mk5en52qmw32fh7uo7.html?fresh=true&title=true&access_token=pk.eyJ1IjoiYnl0aGViYXlkb3Rjb29sIiwiYSI6ImNqajh5eDEwbTMxOXIza3Q0dmhxNnowemkifQ.kgcdaQINw8_7719QVmmT-w#13.8/37.750205/-122.434012/0)
 
 data from: [SF data](https://data.sfgov.org/Geographic-Locations-and-Boundaries/Current-Supervisor-Districts/8nkz-x4ny)
@@ -18,18 +20,5 @@ Install dependencies
 npm install
 ```
 
-Run python simple server
-```python -m SimpleHTTPServer 8000```
-
-navigate to `localhost:8000` in browser
-
-## To develop locally
-Requires browserify and watchify install
-```
-npm install -g browserify
-npm install -g watchify
-```
-Make sure to run browserify if making changes to `app.js`
-```
-watchify app.js -o bundle.js -v
-```
+Run webpack hotload server
+```npm run start```
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+   contentBase: './dist'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          "css-loader",
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
